ec43f2463e6c045e8dc71e3841dcbbd8d094052a
Héloïse Finoux-Nikolic <hf2810@hotmail.com>
2020-06-26 14:35:54 +0200

    ajout de Log4j2 et des plugins Surefire et JaCoCo

--- src/main/java/com/hfinoux/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/hfinoux/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -1,13 +1,25 @@
 package com.hfinoux.SafetyNetAlerts;
 
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SafetyNetAlertsApplication {
 
+	private static Logger logger = LogManager.getLogger(SafetyNetAlertsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+
+
+		logger.debug("Hello from Log4j 2");
+		logger.debug("This is a Debug Message!");
+		logger.info("This is an Info Message!");
+		logger.error("And here comes the Error Message!");
 	}
 
 }
