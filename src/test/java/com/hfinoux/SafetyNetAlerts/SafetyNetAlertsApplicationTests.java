d3f04c53e9e86399a0f5a631f44a3adbb8f96dcf
Héloïse Finoux-Nikolic <hf2810@hotmail.com>
2020-06-26 09:33:52 +0200

    initialisation

---
+++ src/test/java/com/hfinoux/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfinoux.SafetyNetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
